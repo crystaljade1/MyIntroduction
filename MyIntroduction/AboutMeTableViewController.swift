057c56718a5d45f430a01ad74f2462a428830880
Crystal Jade Allen-Washington <crystaljade@macbooks-air.attlocal.net>
2017-05-17 23:54:59 -0400

    Initial commit - introduction in progress.

---
+++ MyIntroduction/AboutMeTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AboutMeTableViewController.swift
+//  MyIntroduction
+//
+//  Created by Crystal Jade Allen-Washington on 5/10/17.
+//  Copyright © 2017 Crystal Jade Allen-Washington. All rights reserved.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController, UITableViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MyIntroduction/TableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MyIntroduction
+//
+//  Created by Crystal Jade Allen-Washington on 5/10/17.
+//  Copyright © 2017 Crystal Jade Allen-Washington. All rights reserved.
+//
+
+import UIKit
+
+
+class TableViewController: UITableViewController {
+    
+   
+    @IBAction func showInterests(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showHometown(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showLanguagesSpoken(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showFavoriteTVShows(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showMadLib(_ sender: UIButton) {
+        
+    }
+
+    
+    
+
+}
+
